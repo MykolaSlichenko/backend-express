2b5931e66ea07bdcd03700aabc5b81e6f373489a
Niklas <nikolaiiislichenko@gmail.com>
2023-09-11 18:15:45 +0300

    setting up basic requests on the server

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 const express = require('express');
-
 const app = express();
+const port = 3003;
+const cors = require('cors');
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send('Successful response.');
+  res.send('Got a GET response.');
 });
 
 app.post('/', (req, res) => {
   res.send('Got a POST request')
-})
+});
 
 app.put('/user', (req, res) => {
   res.send('Got a PUT request at /user')
-})
+});
 
 app.delete('/user', (req, res) => {
   res.send('Got a DELETE request at /user')
-})
+});
 
-app.listen(8080, () => console.log('Example app is listening on port 8080.'));
+app.listen(port, () => {
+  console.log(`Example app is listening on port ${port}!`)
+});
